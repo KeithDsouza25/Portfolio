5f93ee09fd68d3b4de88a5e74dc8c84382a031da
Keith Dsouza <75474616+KeithDsouza25@users.noreply.github.com>
2022-01-18 13:27:16 +0530

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,21 +1,9 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-<<<<<<< HEAD
 import { CgCPlusPlus } from "react-icons/cg";
 import {DiJavascript, DiReact, DiNodejs, DiMongodb, DiPython, DiGit,} from "react-icons/di";
 import { SiPytorch, SiTensorflow, SiFirebase, SiPostgresql, SiJava } from "react-icons/si";
 import { FaDatabase } from "react-icons/fa";
-=======
-import {
-  DiJavascript1,
-  DiReact,
-  DiNodejs,
-  DiMongodb,
-  DiPython,
-  DiGit,
-} from "react-icons/di";
-import { SiOpencv, SiPytorch, SiTensorflow, SiFirebase } from "react-icons/si";
->>>>>>> b5e3f2719b15781383daa2a909dfe905c2f29b00
 
 function Techstack() {
   return (
@@ -24,22 +12,18 @@ function Techstack() {
         <DiPython />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-<<<<<<< HEAD
         <SiPytorch />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         < FaDatabase />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-=======
->>>>>>> b5e3f2719b15781383daa2a909dfe905c2f29b00
         <SiTensorflow />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-<<<<<<< HEAD
         <SiPostgresql />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
@@ -47,10 +31,7 @@ function Techstack() {
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <CgCPlusPlus />
-=======
-        <SiOpencv />
->>>>>>> b5e3f2719b15781383daa2a909dfe905c2f29b00
-      </Col>
+     </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript />
       </Col>
@@ -61,13 +42,10 @@ function Techstack() {
         <DiReact />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
-<<<<<<< HEAD
-=======
         <DiGit />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
->>>>>>> b5e3f2719b15781383daa2a909dfe905c2f29b00
-        <SiFirebase />
+       <SiFirebase />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiJava />
